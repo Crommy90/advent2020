aba5a45eb81b835d36656285a73070bf5e172a18
joshua_crompton <josh.m.crompton@gmail.com>
2020-12-14 18:22:58 +0000

    Day 6, part 2 complete

--- 6/advent2020_6.c
+++ 6/advent2020_6.c
@@ -4,23 +4,27 @@
 
 struct LetterHit
 {
-	bool m_values[26];
+	int m_values[26];
 };
 
 void reset_letter( struct LetterHit* letters )
 {
 	for( int i = 0; i < 26; ++i )
 	{
-		letters->m_values[i] = false;
+		letters->m_values[i] = 0;
 	}
 }
 
-int get_letter_hit_count( struct LetterHit* letters )
+int get_letter_hit_count( struct LetterHit* letters, int count_check )
 {
+	if( count_check == 0 )
+	{
+		return 0;
+	}
 	int count = 0;
 	for( int i = 0; i < 26; ++i )
 	{
-		if( letters->m_values[i] )
+		if( letters->m_values[i] == count_check )
 		{
 			++count;
 		}
@@ -39,25 +43,28 @@ void process_file( const char* file_name )
 	struct LetterHit letters;
 	reset_letter( &letters );
 	int count = 0;
+	int num_passengers = 0;
 	while( fgets(buf, 1000, file_ptr) != NULL )
 	{
 		if( buf[0]== '\n' )
 		{
-			count += get_letter_hit_count( &letters );
+			count += get_letter_hit_count( &letters, num_passengers );
 			reset_letter( &letters );
+			num_passengers = 0;
 			continue;
 		}
+		++num_passengers;
 		int index = 0;
 		while( isalpha( buf[index] ) )
 		{
 			// We're storing ASCII, a is 97
 			int as_ascii = (int) buf[index];
 			as_ascii -= 97;
-			letters.m_values[as_ascii] = true;
+			letters.m_values[as_ascii]++;
 			++index;
 		}
 	}
-	count += get_letter_hit_count( &letters );
+	count += get_letter_hit_count( &letters, num_passengers );
 	printf( "The total sum is %d\n", count );
 	fclose( file_ptr );	
 }
