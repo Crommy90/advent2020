a205d2d5b9932a86293a601e9537fca752ace904
joshua_crompton <josh.m.crompton@gmail.com>
2020-12-21 15:58:35 +0000

    Day 7, part 2

--- 7/advent_7.py
+++ 7/advent_7.py
@@ -1,7 +1,8 @@
 import os
 import string
 
-def run_program( file_name : str ):
+
+def get_bag_dictionary( file_name : str) :
     p_file = open( file_name , 'r')
     lines = p_file.readlines()
     bag_dictionary = {}
@@ -18,23 +19,44 @@ def run_program( file_name : str ):
                 bag_name = other_bags[i+1] + other_bags[i+2]
                 other_bag_dict[bag_name] = bag_num
             bag_dictionary[bag] = other_bag_dict
-    print( bag_dictionary )
+    #print( bag_dictionary )
+    return bag_dictionary
 
+def run_part_1( file_name : str ):
+    bag_dictionary = get_bag_dictionary( file_name )
     list_to_check = ["shinygold"]
     successful_bags = set()
     while len( list_to_check ) != 0:
         for bag in bag_dictionary:
             if list_to_check[0] in bag_dictionary[bag] and list_to_check[0] != bag:
-                print( list_to_check[0] + " found in " + bag )
+                #print( list_to_check[0] + " found in " + bag )
                 list_to_check.append( bag )
                 successful_bags = successful_bags.union( { bag } )
         list_to_check = list_to_check[1:]
-    print( str( successful_bags ))
+    #print( str( successful_bags ))
     print( "Number bags: " + str(len( successful_bags)))
+
+def get_num_bags( bag : str, bag_dict : dict, mul : int ):
+    num_bags = mul # this is self
+    for sub_bag in bag_dict[bag]:
+        num_bags += mul * get_num_bags( sub_bag, bag_dict, bag_dict[bag][sub_bag] )
+    #print( str( num_bags) + " bags in bag " + bag )
+    return num_bags
+
+def run_part_2( file_name : str ):
+    bag_dictionary = get_bag_dictionary( file_name )
+    num_bags = get_num_bags( "shinygold", bag_dictionary, 1 ) - 1 #Minus 1 as it will include self
+    print( "Number bags: " + str( num_bags) )
              
 
 print( "Running Example Data")
-run_program( "./data/sample.txt")
+run_part_1( "./data/sample.txt")
 
 print( "Running Actual Data")
-run_program( "./data/actual.txt")
+run_part_1( "./data/actual.txt")
+
+print( "Running Example Data")
+run_part_2( "./data/sample.txt")
+
+print( "Running Actual Data")
+run_part_2( "./data/actual.txt")
